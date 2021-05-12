5ee7c769646d00ccd0f5050ac1f10564288d7b9c
riyadhswe <riyadhswe@gmail.com>
2021-05-12 18:55:22 +0600

    update

--- main.py
+++ main.py
@@ -8,15 +8,15 @@ time.sleep(5)
 number = [1, 2]
 bit = [7]
 while keyboard.is_pressed('t') == False:
-    if pyautogui.pixel(1692, 720)[0] == 120:
+    if pyautogui.pixel(1577, 1027)[0] == 119:
         if range(random.choice(number) != 1):
             for i in range(random.choice(bit)):
-                pyautogui.click(x=805, y=691)
-            pyautogui.click(x=902, y=692)
+                pyautogui.click(x=1664, y=1001)
+            pyautogui.click(x=1577, y=1027)
         else:
             for i in range(random.choice(bit)):
-                pyautogui.click(x=1754, y=694)
-            pyautogui.click(x=1685, y=689)
+                pyautogui.click(x=235, y=1010)
+            pyautogui.click(x=361, y=1020)
 
 
             """
